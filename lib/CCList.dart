3c05f9f6acb914efd5b2d4615c73d2067543738c
Dmitry <dmitryafitserov@gmail.com>
2020-10-22 14:30:21 +0300

    Created CCListState for list state in body main view. Created data class

--- lib/CCList.dart
+++ lib/CCList.dart
@@ -1,14 +1,47 @@
+import 'package:cctracker/CCData.dart';
 import 'package:flutter/material.dart';
 
-class CCList extends StatelessWidget{
+class CCList extends StatefulWidget{
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Awesome CC Tracker'),
-      ),
-      body: Text('I am body')
-    );
+  State createState() {
+    return CCListState();
   }
 }
+
+
+  class CCListState extends State<CCList>{
+
+    List<CCData> data = [];
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Awesome CC Tracker'),
+        ),
+        body: Container(
+          child: ListView(
+            children: _buildList(),
+          ),
+        )
+      );
+    }
+
+    List<Widget> _buildList(){
+
+      return data.map((CCData f) => ListTile(
+        subtitle: Text(f.symbol),
+        title: Text(f.name),
+        leading: CircleAvatar(child: Text(f.rank.toString())),
+        trailing: Text('\$${f.price.toString()}'),
+
+      )).toList();
+
+    }
+
+  }
+
+
+
+

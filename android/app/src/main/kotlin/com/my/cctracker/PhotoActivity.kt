177dce3fb4948bb6957de22b8d62979c540de9d6
Dmitry <dmitryafitserov@gmail.com>
2020-11-12 14:26:46 +0300

    added in kotlin app fun for get image from gallery and set to imageView

--- android/app/src/main/kotlin/com/my/cctracker/PhotoActivity.kt
+++ android/app/src/main/kotlin/com/my/cctracker/PhotoActivity.kt
@@ -1,36 +1,50 @@
 package com.my.cctracker
 
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.my.cctracker.singleton.Singleton
 
+
 class PhotoActivity : AppCompatActivity() {
+
+    private val PERMISSION_CODE_READ = 1001
+    private val PERMISSION_CODE_WRITE = 1002
+    private val IMAGE_PICK_CODE = 1000
+
 
     lateinit var photoButton: Button
     lateinit var galleryButton: Button
     lateinit var applyButton: Button
+    lateinit var imageView: ImageView
 
     private val clickListener = View.OnClickListener { view ->
 
         when (view.id) {
 
             R.id.button_photo -> {
-                print("click button_photo")
+
                 photoButton.setText("photoButton")
 
             }
             R.id.button_gallery -> {
-                print("click button_gallery")
-                galleryButton.setText("galleryButton")
+
+                getPhotoFromGallery()
             }
             R.id.button_apply -> {
-                print("click button_apply")
-                applyButton.setText("applyButton")
+
+                Singleton.mch?.success("back_without_save")
+                finish()
             }
 
         }
@@ -44,6 +58,9 @@ class PhotoActivity : AppCompatActivity() {
         photoButton = findViewById(R.id.button_photo)
         galleryButton = findViewById(R.id.button_gallery)
         applyButton = findViewById(R.id.button_apply)
+
+        imageView = findViewById(R.id.image_view_photo)
+
         photoButton.setOnClickListener(clickListener)
         galleryButton.setOnClickListener(clickListener)
         applyButton.setOnClickListener(clickListener)
@@ -51,8 +68,9 @@ class PhotoActivity : AppCompatActivity() {
         var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setActionBar(toolbar)
 
-        getSupportActionBar()!!.setHomeAsUpIndicator(R.mipmap.ic_launcher);// set drawable icon
+        getSupportActionBar()!!.setHomeAsUpIndicator(R.drawable.button_back_v1);
         getSupportActionBar()!!.setDisplayHomeAsUpEnabled(true);
+
 
     }
 
@@ -74,4 +92,50 @@ class PhotoActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    private fun getPhotoFromGallery(){
+    if(checkPermissionForImage()){
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+    }
+
+
+    }
+
+    private fun checkPermissionForImage() :Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                    && (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            ) {
+                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                val permissionCoarse = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+
+                requestPermissions(permission, PERMISSION_CODE_READ) // GIVE AN INTEGER VALUE FOR PERMISSION_CODE_READ LIKE 1001
+
+                requestPermissions(permissionCoarse, PERMISSION_CODE_WRITE) // GIVE AN INTEGER VALUE FOR PERMISSION_CODE_WRITE LIKE 1002
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return true
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+
+            imageView.setImageURI(data?.data)
+        } else if(resultCode == Activity.RESULT_OK && requestCode == PERMISSION_CODE_READ){
+            getPhotoFromGallery()
+        }
+    }
+
+
 }

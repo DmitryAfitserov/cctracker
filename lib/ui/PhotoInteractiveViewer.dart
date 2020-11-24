bb0b0fdcd994964915f5f5eb3b3b37de699341d1
Dmitry <dmitryafitserov@gmail.com>
2020-11-24 18:21:14 +0300

    created class PhotosInteractiveViewer for open photo full screen and zoom photo

--- lib/ui/PhotoInteractiveViewer.dart
+++ lib/ui/PhotoInteractiveViewer.dart
@@ -4,6 +4,8 @@ import 'package:cctracker/ui/widget/ItemPhoto.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:io';
+
 
 class PhotosInteractiveViewer extends StatefulWidget {
 
@@ -23,11 +25,37 @@ class PhotosInteractiveViewerState extends State<PhotosInteractiveViewer> {
 
   @override
   Widget build(BuildContext context) {
-
+    final TransformationController _controller = TransformationController();
     print("path = " + widget.path);
 
+
     return Scaffold(
-      body: Text("df"),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body:       Center(
+        child: InteractiveViewer(
+          //  boundaryMargin: EdgeInsets.all(20.0),
+          minScale: 1,
+          maxScale: 3,
+          transformationController: _controller,
+          panEnabled: false,
+
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: FileImage(File( widget.path))
+                ,
+                //   fit: BoxFit.fill,
+              ),
+              // shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      ),
     );
+
+
   }
 }
